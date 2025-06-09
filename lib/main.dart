50065070df0cae50905dc359fabd8fad9baef22b
Jayraj <jayrajsinh@euphotic.io>
2025-06-09 16:16:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const UserManualApp());
+}
+
+class UserManualApp extends StatelessWidget {
+  const UserManualApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User Manual',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const UserManualPage(),
+    );
+  }
+}
+
+class UserManualPage extends StatelessWidget {
+  const UserManualPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Manual'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to the User Manual!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
