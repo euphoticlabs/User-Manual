7a3fa4213c6c3d57ee6c863bf83c9ab0ad55c788
Jayraj <jayrajsinh@euphotic.io>
2025-07-04 15:10:57 +0530

    Refactor asset URL management in constant.dart and added all images in assets

--- lib/global/constant.dart
+++ lib/global/constant.dart
@@ -1,19 +1,21 @@
 class R {
   R._();
   static const String baseUrl = "https://nosh-user-manual.s3.ap-south-1.amazonaws.com";
-  static const String homeS3bucket = "$baseUrl/home/";
-  static const String knowYourNosh = "$baseUrl/know-nosh/";
-  static const String components = "$baseUrl/components/";
-  static const String spice = "$baseUrl/spice/";
-  static const String ingredients = "$baseUrl/ingTray/";
-  static const String liquid = "$baseUrl/liquid/";
-  static const String chimney = "$baseUrl/chimney/";
-  static const String stirrer = "$baseUrl/stirrer/";
-  static const String induction = "$baseUrl/induction/";
-  static const String pan = "$baseUrl/pan/";
-  static const String sensors = "$baseUrl/sensors/";
-  static const String dayTodayCleaning = "$baseUrl/cleaning-d2d/";
-  static const String monthlyCleaning = "$baseUrl/cleaning-m/";
-  static const String fourMonthsCleaning = "$baseUrl/cleaning-4m/";
-  static const String cabinetInstallation = "$baseUrl/cabinet_installation/";
+  static const String assets = "assets/images/";
+  static const String currentURL = assets;
+  static const String homeS3bucket = "$currentURL/home/";
+  static const String knowYourNosh = "$currentURL/know-nosh/";
+  static const String components = "$currentURL/components/";
+  static const String spice = "$currentURL/spice/";
+  static const String ingredients = "$currentURL/ingTray/";
+  static const String liquid = "$currentURL/liquid/";
+  static const String chimney = "$currentURL/chimney/";
+  static const String stirrer = "$currentURL/stirrer/";
+  static const String induction = "$currentURL/induction/";
+  static const String pan = "$currentURL/pan/";
+  static const String sensors = "$currentURL/sensors/";
+  static const String dayTodayCleaning = "$currentURL/cleaning-d2d/";
+  static const String monthlyCleaning = "$currentURL/cleaning-m/";
+  static const String fourMonthsCleaning = "$currentURL/cleaning-4m/";
+  static const String cabinetInstallation = "$currentURL/cabinet_installation/";
 }
